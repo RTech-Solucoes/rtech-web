0c81ee3fed1f91e940e443a0831efc087af47665
luizrxg-rtech <luiz.gomes@rtechsolution.com.br>
2025-06-09 19:08:43 -0300

    att

--- src/components/Solutions.tsx
+++ src/components/Solutions.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Monitor, MessageSquare, BarChart3 } from 'lucide-react';
 import { useLanguage } from '@/contexts/LanguageContext';
-import {Carousel} from "@/components/ui/carousel.tsx";
+import {Carousel, CarouselItem} from "@/components/ui/carousel.tsx";
 
 const Solutions = () => {
   const { t } = useLanguage();
@@ -43,7 +43,7 @@ const Solutions = () => {
     <section id="solutions" className="pb-20 pt-40 bg-gradient-to-b from-primary-light to-primary relative overflow-hidden">
       <div className="absolute bottom-40 left-0 w-72 h-72 bg-accent/10 rounded-lg blur-3xl"></div>
       <div className="absolute bottom-0 right-0 w-96 h-96 bg-primary/30 rounded-lg blur-3xl"></div>
-      
+
       <div className="flex flex-col items-center relative max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 space-y-56">
         <div className="grid lg:grid-cols-2 gap-16 items-center">
           <div className="space-y-8">
@@ -83,8 +83,8 @@ const Solutions = () => {
             ))}
           </div>
         </div>
-        <div className="flex flex-col gap-4 bg-foreground/10 backdrop-blur-xl p-8 rounded-2xl w-fit" data-aos="zoom-out">
-          <div className="flex items-center justify-between">
+        <div className="flex flex-col bg-foreground/10 backdrop-blur-xl rounded-2xl w-fit" data-aos="zoom-out">
+          <div className="flex items-center justify-between pt-8 pb-7 mx-8 border-b border-foreground/20 ">
             <div className="text-foreground text-2xl font-semibold">{t('solutions_dashboard')}</div>
             <div className="flex space-x-2">
               <div className="w-3 h-3 bg-red-400 rounded-lg"></div>
@@ -93,18 +93,17 @@ const Solutions = () => {
             </div>
           </div>
 
-
-          <Carousel className="border-t border-foreground/20 flex flex-col lg:flex-row gap-24 px-5 pt-6 w-fit">
+          <div className="flex flex-col lg:flex-row gap-24 py-8 px-12 w-fit overflow-hidden">
             {companies.map((company, index) =>
               <img
-                className='mb-auto w-full max-w-80 max-h-36'
+                className='my-auto w-full max-w-80 max-h-36'
                 data-aos="zoom-in"
                 data-aos-delay={400 + (index * 100)}
                 src={company.src}
                 alt={company.name}
               />
             )}
-          </Carousel>
+          </div>
         </div>
       </div>
     </section>
